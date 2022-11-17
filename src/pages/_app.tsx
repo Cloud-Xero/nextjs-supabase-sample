2a4d283a1cf502a10f5b95d02a54dfd757f1f3a4
Cloud-Xero <hiropon201511@gmail.com>
2022-11-18 01:32:41 +0900

    fix: import path

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import { Auth } from "@supabase/ui";
 import type { AppProps } from "next/app";
-import { AuthLayout } from "@/layout/AuthLayout";
-import { client } from "@/libs/supabase";
-import "@/styles/globals.css";
+import { AuthLayout } from "@this/layout/AuthLayout";
+import { client } from "@this/libs/supabase";
+import "tailwindcss/tailwind.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
